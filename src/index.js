36e0f874019ed656f3ec569f07cb406a0233d9ca
Denisyasyuchenya <yasyuchenya1@gmail.com>
2023-02-27 01:04:59 +0300

    chore: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let strLength = str.length;
+  for (let i = 0; i < strLength; i++) {
+    let arrayLength = arr.length;
+    const x = bracketsConfig.find(bracketsConfig => bracketsConfig.includes(str[i]));
+    x[0] === arr[arrayLength - 1] && x[1]=== str[i] ? arr.pop() : arr.push(str[i]);
+  }
+  return (arr.length === 0);
 }
